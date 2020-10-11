6fd072520208697dae2c93c3fa0bb6f256f8e57a
Mohit Kumar <mohitme@users.noreply.github.com>
2020-10-11 09:09:55 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import json
 from difflib import get_close_matches
 data = json.load(open("dictionary.json"))
@@ -31,10 +25,5 @@ if type(out) == list:
         print("-",i)
 else:
     print("-",out)
-
-
-# In[ ]:
-
-
 
 
